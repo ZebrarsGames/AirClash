e8d89a4bce66751155d4aba2490aaf77240a402e
Zebrar <777evgeniy2301@gmail.com>
2026-03-21 21:26:23 +0500

    feat: Добавил управление игрокам

--- Assets/Scripts/PlayersController.cs
+++ Assets/Scripts/PlayersController.cs
@@ -3,21 +3,26 @@ using UnityEngine.EventSystems;
 
 public class PlayersController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-
-    public GameObject player1;
-    public GameObject player2;
     private Vector3 offset;
+    private Rigidbody2D rb;
+
+    void Start()
+{
+    rb = GetComponent<Rigidbody2D>();
+}
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(eventData.position);
-        offset = transform.position - new Vector3(mousePos.x, mousePos.y, 0);
+        offset = transform.position - new Vector3(mousePos.x, mousePos.y);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(eventData.position);
-        transform.position = new Vector3(mousePos.x + offset.x, mousePos.y + offset.y, transform.position.z);
+        Vector2 targetPos = new Vector2(mousePos.x + offset.x, mousePos.y + offset.y);
+
+        rb.MovePosition(targetPos);
     }
 
     public void OnEndDrag(PointerEventData eventData)
