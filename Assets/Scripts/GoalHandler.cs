6830a4ce525e6efbfc936e76f651aa55523b45e4
Zebrar <777evgeniy2301@gmail.com>
2026-03-21 22:32:02 +0500

    feat: Сделал систему голов

--- Assets/Scripts/GoalHandler.cs
+++ Assets/Scripts/GoalHandler.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Text scoreText1;
+    [SerializeField] private Text scoreText2;
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+    private Vector2 player1startPos;
+    private Vector2 player2startPos;
+    private Vector2 puckStartPos;
+    public GameObject puck;
+    private int score1 = 0;
+    private int score2 = 0;
+
+    void Awake()
     {
-        
+        player1startPos = player1.transform.position;
+        player2startPos = player2.transform.position;
+        puckStartPos = puck.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("GoalTrigger1"))
+        {
+            score1++;
+            scoreText1.text = score1.ToString();
+            ResetPosition();
+        }
+        else if (collision.gameObject.CompareTag("GoalTrigger2"))
+        {
+            score2++;
+            scoreText2.text = score2.ToString();
+            ResetPosition();
+        }
+    }
+
+    public void ResetPosition()
+    {
+        if (puck != null)
+        {
+            puck.transform.position = puckStartPos;
+            player1.transform.position = player1startPos;
+            player1.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+            player2.transform.position = player2startPos;
+            player2.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+            Rigidbody2D rb = puck.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                rb.linearVelocity = Vector2.zero;
+                rb.angularVelocity = 0f;
+            }
+        }
     }
 }
