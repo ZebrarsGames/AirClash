7e8d435dec2d656b0d409c31113b5217bdfcad8f
Zebrar <777evgeniy2301@gmail.com>
2026-03-21 21:00:26 +0500

    Написал тестовый скрипт на управление игроком. Ещё не тестил

--- Assets/Scripts/PlayersController.cs
+++ Assets/Scripts/PlayersController.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PlayersController : MonoBehaviour
+public class PlayersController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public GameObject player1;
+    public GameObject player2;
+    private Vector3 offset;
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(eventData.position);
+        offset = transform.position - new Vector3(mousePos.x, mousePos.y, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(eventData.position);
+        transform.position = new Vector3(mousePos.x + offset.x, mousePos.y + offset.y, transform.position.z);
     }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Объект отпущен");
+    }
+
 }
