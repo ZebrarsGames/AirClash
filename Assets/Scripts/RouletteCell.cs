3601b02dd2db92e561b06630bd7fb953159a1133
Zebrar <777evgeniy2301@gmail.com>
2026-04-22 11:31:57 +0500

    Добавил больше наград, добавил анимацию в рулетке и добавил звук для рулетки

--- Assets/Scripts/RouletteCell.cs
+++ Assets/Scripts/RouletteCell.cs
@@ -5,6 +5,12 @@ public class RouletteCell : MonoBehaviour
 {
     public Image icon;
     public RouletteItemData currentData; // Здесь хранятся данные конкретной ячейки
+    public RectTransform rectTransform;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
 
     public void SetData(RouletteItemData data)
     {
