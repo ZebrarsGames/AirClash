12aea6cc0b2292980c897b27d8e0c0217b580511
Zebrar <777evgeniy2301@gmail.com>
2026-04-30 10:43:24 +0500

    feat: Добавил функционал тексту в меню в опытом

--- Assets/Scripts/XpUiScr.cs
+++ Assets/Scripts/XpUiScr.cs
@@ -5,9 +5,15 @@ public class XpUiScr : MonoBehaviour
 {
     [SerializeField] private Slider xpSlider;
     [SerializeField] private XpHandler xpHandler;
+    [SerializeField] private Text currentXpText;
+    [SerializeField] private Text currentLvlText;
+    [SerializeField] private Text nextLvlText;
     void Start()
     {
         SetProgress(xpHandler.GetXPProgress());
+        currentXpText.text = xpHandler.GetXP().ToString() + " / " + xpHandler.GetXpToNextLevel().ToString() + " XP";
+        currentLvlText.text = xpHandler.GetLevel().ToString();
+        nextLvlText.text = (xpHandler.GetLevel() + 1).ToString();
     }
 
     public void SetProgress(float progress)
