cb54db21220183205bcc048feb5f005d8d91a4b4
Zebrar <777evgeniy2301@gmail.com>
2026-05-04 12:02:15 +0500

    fix: Исправил баг когда громкость сама менялась при заходе в настройки

--- Assets/Scripts/SettingsHandler.cs
+++ Assets/Scripts/SettingsHandler.cs
@@ -15,9 +15,7 @@ public class SettingsHandler : MonoBehaviour
         QualitySettings.vSyncCount = 0;
         if(PlayerPrefs.GetInt("FPS") == 0) PlayerPrefs.SetInt("FPS", 60);
         else Application.targetFrameRate = PlayerPrefs.GetInt("FPS");
-        if(PlayerPrefs.GetInt("MusicVolume") == 0) PlayerPrefs.SetFloat("MusicVolume", 0.5f);
-        else Application.targetFrameRate = PlayerPrefs.GetInt("MusicVolume");
-        volumeSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        volumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1.0f);
         fpsSlider.value = PlayerPrefs.GetInt("FPS");
         if(SceneManager.GetActiveScene().name == "MainMenu")
         {
