306c91b4190b74dbf7a4525e6eace1d47b12ce3a
Zebrar <777evgeniy2301@gmail.com>
2026-05-13 11:25:58 +0500

    feat: Добавил тестовую выдачу наград

--- Assets/Scripts/QuestsHandler.cs
+++ Assets/Scripts/QuestsHandler.cs
@@ -4,18 +4,44 @@ public class QuestsHandler : MonoBehaviour
 {
     [Header("Arrays")]
     [SerializeField] private QuestSO[] commonQuests;
+    [Header("Scripts")]
+    [SerializeField] private MoneyHandler moneyHandler;
+    [SerializeField] private XpHandler xpHandler;
 
-    public void UpdateQuestProgress(string questName, int amount)
+    public void UpdateQuestProgress(string questId, int amount)
     {
         for(int i = 0; i < commonQuests.Length; i++)
         {
-            if(commonQuests[i].QuestName.Equals(questName))
+            if(commonQuests[i].QuestName.Equals(questId))
             {
                 QuestSO currentQuest = commonQuests[i];
                 QuestSaveSystem.PlusProgress(currentQuest.QuestId, amount);
                 if(QuestSaveSystem.GetProgress(currentQuest.QuestId) >= currentQuest.Target)
                 {
                     QuestSaveSystem.SetCompleted(currentQuest.QuestId);
+                    GiveAward(currentQuest.QuestId);
+                }
+            }
+        }
+    }
+    public void GiveAward(string questId)
+    {
+        for(int i = 0; i < commonQuests.Length; i++)
+        {
+            if(commonQuests[i].QuestName.Equals(questId))
+            {
+                QuestSO currentQuest = commonQuests[i];
+                switch(currentQuest.AwardType)
+                {
+                    case AwardType.Money:
+                        moneyHandler.AddMoney(currentQuest.Award);
+                        break;
+                    case AwardType.Xp:
+                        xpHandler.AddXp(currentQuest.Award);
+                        break;   
+                    case AwardType.Skin:
+                        Debug.Log("Данная награда в разработке!");
+                        break;     
                 }
             }
         }
