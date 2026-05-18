bc544605c5d4343e160efc25ea56fb745bd75b82
Zebrar <777evgeniy2301@gmail.com>
2026-05-18 10:49:08 +0500

    Написал тестовые скрипты для ежедневных квестов

---
+++ Assets/Scripts/DailyQuestItem.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DailyQuestItem : MonoBehaviour
+{
+    [Header("Quest Info")]
+    [SerializeField] private string questId;
+    [Header("UI")]
+    [SerializeField] private Text questNameText;
+    [SerializeField] private Text questDescriptionText;
+    [SerializeField] private Text targetText;
+    [SerializeField] private GameObject completeArrow;
+    [SerializeField] private Image questLogo;
+    public void SetData(DailyQuestSO quest)
+    {
+        questNameText.text = quest.QuestName;
+        questDescriptionText.text = quest.Description;
+        questLogo.sprite = quest.QuestLogo;
+        questId = quest.QuestId;
+    }
+}
