d30dce62d98f71003e3da6db7e4af30e22fa4add
Zebrar <777evgeniy2301@gmail.com>
2026-07-07 18:20:35 +0500

    Добавил анимацию открывания и закрывания паузы

--- Assets/Scripts/PauseHandler.cs
+++ Assets/Scripts/PauseHandler.cs
@@ -1,26 +1,49 @@
 using UnityEngine;
+using DG.Tweening;
+using System.Collections;
 
 public class PauseHandler : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject pausePanel;
     private bool isPaused = false;
     [SerializeField] private GoalHandler goalHandler;
 
     public void TogglePause()
     {
         isPaused = !isPaused;
-        pauseMenu.SetActive(isPaused);
+        AnimatePauseMenu();
+    }
+    private void AnimatePauseMenu()
+    {
         if(isPaused)
+        {  
+            pauseMenu.SetActive(true);
+            StartCoroutine(ShowPauseMenu());
+        } else
         {
-            Time.timeScale = 0f;
-        }
-        else
-        {
-            Time.timeScale = 1f;
+            StartCoroutine(HidePauseMenu());
         }
     }
-
-
+    IEnumerator HidePauseMenu()
+    {
+        Time.timeScale = 1.0f;
+        var rect = pausePanel.GetComponent<RectTransform>();
+        rect.DOScale(Vector3.zero, 0.3f).SetEase(Ease.InBack);
+        yield return new WaitForSeconds(0.35f);
+        pausePanel.SetActive(false);
+        pauseMenu.SetActive(false);
+    }
+    IEnumerator ShowPauseMenu()
+    {
+        Time.timeScale = 1.0f;
+        var rect = pausePanel.GetComponent<RectTransform>();
+        rect.localScale = Vector3.zero;
+        pausePanel.SetActive(true);
+        rect.DOScale(new Vector3(1.0f, 1.0f, 1.0f), 0.3f).SetEase(Ease.OutBack);
+        yield return new WaitForSeconds(0.35f);
+        Time.timeScale = 0f;
+    }
     public void MainMenu()
     {
         PlayerPrefs.SetInt("HowMoneyAdds", 0);
