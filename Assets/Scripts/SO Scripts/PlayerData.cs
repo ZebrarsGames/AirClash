171a304632b145d70c4c1115145dfc88e7b2a998
Zebrar <777evgeniy2301@gmail.com>
2026-07-08 09:34:04 +0500

    Улучшил SaveManager и PlayerData

--- Assets/Scripts/SO Scripts/PlayerData.cs
+++ Assets/Scripts/SO Scripts/PlayerData.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 [Serializable]
 public class PlayerData : ScriptableObject
 {
+    public string NickName;
+    public string AvatarPath;
     public int Money;
     public int XP;
     public int XpLevel;
     public int Goals;
-    public string[] UnlockedSkinsIds; //coming soon
-    public string[] UnlockedAchievementsIds; //coming soon
+    public SkinData CurrentSkin;
 }
