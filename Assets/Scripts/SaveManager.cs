11ef62a215860c3e6870943858f27788cc8c0f44
Zebrar <777evgeniy2301@gmail.com>
2026-07-08 09:34:04 +0500

    Улучшил SaveManager и PlayerData

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -12,8 +12,35 @@ public static class SaveManager
         playerData.XP = PlayerPrefs.GetInt("CurrentXp", 0);
         playerData.XpLevel = PlayerPrefs.GetInt("XpLevel", 1);
         playerData.Goals = PlayerPrefs.GetInt("TotalGoals", 0);
+        playerData.NickName = "test";
+        playerData.AvatarPath = "test";
 
         string json = JsonUtility.ToJson(playerData);
         File.WriteAllText(Application.persistentDataPath + "/save.json", json);
+    }
+
+    public static PlayerData GetData()
+    {
+        if (!File.Exists(Application.persistentDataPath + "/save.json"))
+        {
+            Debug.Log("No save file");
+            return new PlayerData();
+        }
+        string json = File.ReadAllText(Application.persistentDataPath + "/save.json");
+        PlayerData playerData = JsonUtility.FromJson<PlayerData>(json);
+        return playerData;
+    }
+
+    public static void DeleteData()
+    {
+        if (File.Exists(Application.persistentDataPath + "/save.json"))
+        {
+            File.Delete(Application.persistentDataPath + "/save.json");
+            Debug.Log("Файл сохранения успешно удален по пути: " + Application.persistentDataPath + "/save.json");
+        }
+        else
+        {
+            Debug.LogWarning("Файл сохранения не найден");
+        }
     }
 }
