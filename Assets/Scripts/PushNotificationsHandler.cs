35c1412d466070f0c3eb749057b52b653daec154
Zebrar <777evgeniy2301@gmail.com>
2026-07-19 18:41:30 +0500

    Добавил заготовку под уведомления. Уведомления я сделаю в другом апдейте

---
+++ Assets/Scripts/PushNotificationsHandler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Unity.Notifications.Android;
+
+public class PushNotificationsHandler : MonoBehaviour
+{
+    void Awake()
+    {
+        AndroidNotificationChannel channel = new AndroidNotificationChannel()
+        {
+            Name = "News | Новости",
+            Description = "Уведомления о новостях игры.",
+            Id = "news",
+            Importance = Importance.Default
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(channel);
+    }
+
+    public void SendNotification()
+    {
+        AndroidNotification notification = new AndroidNotification()
+        {
+            Title = "Заголовок",
+            Text = "Текст уведомления",
+            FireTime = System.DateTime.Now.AddSeconds(5),
+            SmallIcon = "small_icon",
+            LargeIcon = "large_icon"
+        };
+
+        AndroidNotificationCenter.SendNotification(notification, "news");
+    }
+}
